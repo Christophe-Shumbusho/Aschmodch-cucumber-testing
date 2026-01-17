1dfb3836269143e3f7de5a0f24a70f9ce9f6e7c3
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2026-01-18 00:05:00 +0200

    Driver factory initialized

---
+++ src/test/java/Factory/DriverFactory.java
@@ -0,0 +1,31 @@
+package Factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    private static WebDriver driver;
+    public static WebDriver initializeDriver(String browser){
+        switch(browser){
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver","resource/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver","resource/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new IllegalStateException("INVALID BROWSER" + browser);
+
+        }
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+}
